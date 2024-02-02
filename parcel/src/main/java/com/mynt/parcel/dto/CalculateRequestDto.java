9138c511eb65e805e5448f6797c17af6a1be8abe
john.viray <john.viray@tagitmobile.com>
2024-02-02 18:02:46 +0800

    MYNT-01 - Changed Validation

--- parcel/src/main/java/com/mynt/parcel/dto/CalculateRequestDto.java
+++ parcel/src/main/java/com/mynt/parcel/dto/CalculateRequestDto.java
@@ -1,7 +1,9 @@
 package com.mynt.parcel.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -16,28 +18,32 @@ public class CalculateRequestDto {
 			description="Parcel's weight by Kilogram",
 			example="5"
 			)
-	@NotEmpty(message="Weight cannot be null or empty")
+	@Digits(integer=10, fraction=2)
+	@Positive 
 	private double weight;
 	
 	@Schema(
 			description="Parcel's height by Centimeter",
 			example="5"
 			)
-	@NotEmpty(message="Height cannot be null or empty")
+	@Digits(integer=10, fraction=2)
+	@Positive 
 	private double height;
 	
 	@Schema(
 			description="Parcel's width by Centimeter",
 			example="5"
 			)
-	@NotEmpty(message="Width cannot be null or empty")
+	@Digits(integer=10, fraction=2)
+	@Positive 
 	private double width;
 	
 	@Schema(
 			description="Parcel's length by Centimeter",
 			example="5"
 			)
-	@NotEmpty(message="Length cannot be null or empty")
+	@Digits(integer=10, fraction=2)
+	@Positive 
 	private double length;
 	
 	@Schema(
